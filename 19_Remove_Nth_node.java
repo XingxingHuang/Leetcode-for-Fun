74b983c70649d5f772a7eab2405915b1e057fe76
XingxingHuang <phoebe800@gmail.com>
2017-02-10 11:34:59 -0600

    add reforece method

--- 19_Remove_Nth_node.java
+++ 19_Remove_Nth_node.java
@@ -1,11 +1,25 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
+ public class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode result = new ListNode(0);
+        result.next = head;
+        ListNode helper = result;
+        // get the size 
+        int m = 0;
+        while (head != null) {
+            m ++;
+            head = head.next;
+        }
+        // move to the node 
+        if (m <= 1) {return null;}
+        for (int i = 1; i < m - n + 1; i++) {
+            helper = helper.next;
+            
+        }
+        // delete the node 
+        helper.next = helper.next.next;
+        return result.next;
+    }
+}
 public class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
         if (head == null || head.next == null) {
@@ -23,7 +37,7 @@ public class Solution {
             node = node.next;
             helper = helper.next;
         }
-        // ignore the helper.next;
+        // delete the nth node, node.next;
         node.next  = node.next.next;
         return result.next;
     }
