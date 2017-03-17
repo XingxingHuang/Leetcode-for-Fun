922eed2fb58eb6fb6fa20e372c3f02ca68c46c0d
XingxingHuang <phoebe800@gmail.com>
2017-03-17 16:06:20 +0800

    add

---
+++ 240_Search_2D_Matrix_II.java
@@ -0,0 +1,21 @@
+// @Author: Xingxing Huang
+// Time: O(m) + O(n)
+public class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+        int i = 0;
+        int j = matrix[0].length - 1;
+        while (i < matrix.length && j >= 0) {
+            if (target < matrix[i][j]) {
+                j--;
+            } else if (target > matrix[i][j]) {
+                i++;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
