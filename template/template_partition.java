456a3bfa68f2c4b38e33d1d071c98f0ee1e4a2ec
XingxingHuang <phoebe800@gmail.com>
2017-06-17 04:44:11 +0800

    template

--- template/template_partition.java
+++ template/template_partition.java
@@ -19,12 +19,31 @@ public int partition (int[] nums, int l, int r) {
         }
         nums[left] = nums[right];
         while (left < right && nums[left] <= privot) {
-            left++''
+            left++;
         }
         nums[right] = nums[left];
     }
     
     // 返还pivot点到数组里面
-    nums[pivot] = pivot;
+    nums[left] = pivot;
+    // 继续partition
     return left;
 }
+
+// three way partition
+// https://en.wikipedia.org/wiki/Dutch_national_flag_problem#Pseudocode
+procedure three-way-partition(A : array of values, mid : value):
+    i ← 0
+    j ← 0
+    n ← size of A - 1
+
+    while j ≤ n:
+        if A[j] < mid:
+            swap A[i] and A[j]
+            i ← i + 1
+            j ← j + 1
+        else if A[j] > mid:
+            swap A[j] and A[n]
+            n ← n - 1
+        else:
+            j ← j + 1
