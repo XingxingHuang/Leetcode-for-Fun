baf5fe5e4058b9b589b2eeabae801c890a32b48c
Xingxing Huang <phoebe800@gmail.com>
2017-07-23 21:07:25 -0500

    sql

---
+++ 627_Swap_Salary.sql
@@ -0,0 +1,10 @@
+UPDATE salary SET sex = IF(sex = 'm', 'f', 'm')
+
+
+update salary set sex = CHAR(ASCII('f') ^ ASCII('m') ^ ASCII(sex));
+
+UPDATE salary
+    SET sex  = (CASE WHEN sex = 'm' 
+        THEN  'f' 
+        ELSE 'm' 
+        END)
