a74ad0dfc157d025922e3d5fb319a554fd64ed84
XingxingHuang <phoebe800@gmail.com>
2017-07-24 21:41:28 -0500

    daily

--- 648_Replace_Words.py
+++ 648_Replace_Words.py
@@ -1,3 +1,24 @@
+#最最简单的做法，但是时间复杂度高一点
+class Solution(object):
+    def replaceWords(self, dict, sentence):
+        dict = set(dict)
+        res = []
+        def add(word):
+            for i in range(1, len(word)):
+                if word[:i] in dict:
+                    res.append(word[:i])
+                    return
+            res.append(word)
+            
+        words = sentence.split(" ")
+        for word in words:
+            add(word);
+        return " ".join(res)
+        
+        
+
+
+# 用trie tree
 class Solution(object):
     def replaceWords(self, dict, sentence):
         """
