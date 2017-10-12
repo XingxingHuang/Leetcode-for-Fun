d03957b1f22fa76600ed945176e4b85accabe304
XingxingHuang <phoebe800@gmail.com>
2017-10-11 21:51:08 -0500

    add leetcode medium

--- leetcode/003_Longest_Substring.java
+++ leetcode/003_Longest_Substring.java
@@ -15,3 +15,20 @@ class Solution {
         return res;
     }
 }
+
+// 10.11 Using hashmap
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int[] map = new int[256];
+        Arrays.fill(map, -1);
+        int max = 0;
+        int len = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int idx = s.charAt(i) - ' ';
+            len = (map[idx] == -1) ? len + 1 : Math.min(i - map[idx], len + 1);
+            map[idx] = i;
+            max = Math.max(len, max);
+        }
+        return max;
+    }
+}
