5e5bb86db293384b9142c823d2fa28ffe0000065
Xingxing Huang <phoebe800@gmail.com>
2017-12-22 23:55:12 -0600

    leetcode easy

--- leetcode/303_Range_Sum.java
+++ leetcode/303_Range_Sum.java
@@ -34,3 +34,33 @@ public class NumArray {
  * NumArray obj = new NumArray(nums);
  * int param_1 = obj.sumRange(i,j);
  */
+
+
+// 12.21
+class NumArray {
+    private int[] sums;
+    
+    public NumArray(int[] nums) {
+        if (nums == null || nums.length == 0) { 
+            sums = new int[0];
+            return;
+        }
+        sums = new int[nums.length];
+        sums[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) 
+            sums[i] = sums[i - 1] + nums[i];
+    }
+    
+    public int sumRange(int i, int j) {
+        if (i == 0)
+            return sums[j];
+        else
+            return sums[j] - sums[i - 1];
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(i,j);
+ */
