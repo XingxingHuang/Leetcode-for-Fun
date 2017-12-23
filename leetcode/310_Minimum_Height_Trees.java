2ab9fc45106d36426f87a9a29a97dfece36b202f
Xingxing Huang <phoebe800@gmail.com>
2017-12-22 23:49:20 -0600

    medium

--- leetcode/310_Minimum_Height_Trees.java
+++ leetcode/310_Minimum_Height_Trees.java
@@ -44,3 +44,42 @@ public class Solution {
         return leaves;
     }
 }
+
+
+// 12.21 
+// 错误思路(无法解决nodes重复访问)，先找到枝叶，然后顺着枝叶往里面找，每次更新路径上node的height。最后遍历nodes，返回height等于最大值的那些nodes。
+// 改进，从枝叶开始遍历是，每次删除访问过枝叶，然后访问之后的枝叶。
+class Solution {
+    public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+        // corner case
+        if (n == 1) 
+            return new ArrayList<Integer>(Arrays.asList(0));  // Collections.singletonList(0);
+        // 创建图，用邻接点记录
+        List<Set<Integer>> graph = new ArrayList<>(n);
+        for (int i = 0; i < n; i++) 
+            graph.add(new HashSet<>());
+        for (int[] edge : edges) {
+            graph.get(edge[0]).add(edge[1]);
+            graph.get(edge[1]).add(edge[0]);
+        }    
+        // 找出枝叶
+        List<Integer> leaves = new ArrayList<>();
+        for (int i = 0; i < n; i++)
+            if (graph.get(i).size() == 1) {
+                leaves.add(i);
+            }
+        // 遍历枝叶
+        while (n > 2) {
+            n -= leaves.size();
+            List<Integer> newleaves = new ArrayList<>();
+            for (int node: leaves) {
+                int i = graph.get(node).iterator().next();
+                graph.get(i).remove(node);
+                if (graph.get(i).size() == 1) 
+                    newleaves.add(i);
+            }
+            leaves = newleaves;
+        }
+        return leaves;
+    }
+}
